3d03deba986601881863a9a0ae8e2121ef6b851e
Dennis <148398668+Sikkepit@users.noreply.github.com>
2026-03-20 20:30:01 +0100

    adds contact-info

--- app/_components/footer/Footer.tsx
+++ app/_components/footer/Footer.tsx
@@ -3,7 +3,12 @@ import "./footer.css";
 export default function Footer() {
 	return (
 		<footer className="footer">
-			<section>© 2026 Winparts.nl</section>
+			<section>
+				© 2026 Winparts.nl |{" "}
+				<a href="https://github.com/Sikkepit" target="_blank" className="underline">
+					Dennis Hemstra op Github
+				</a>
+			</section>
 		</footer>
 	);
 }
