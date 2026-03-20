bcd2134211a67a5dd89c8f41dca3d105162648a4
Dennis <148398668+Sikkepit@users.noreply.github.com>
2026-03-20 03:02:55 +0100

    working

--- app/_components/overview/product/PriceContainer.tsx
+++ app/_components/overview/product/PriceContainer.tsx
@@ -7,14 +7,13 @@ export default function PriceContainer({ product }: { product: ProductType }) {
 	return (
 		<div className="product-card__price-container">
 			<div className="flex flex-col items-end py-3 gap-1 h-full">
+				{product.suggestedPrice && <WinPrijs />}
+
 				<div className="flex flex-col items-end px-2">
 					{product.suggestedPrice && (
-						<>
-							<WinPrijs />
-							<span className="product-card__adviesprijs">
-								Adviesprijs {displayAsCurrency(product.suggestedPrice)}
-							</span>
-						</>
+						<span className="product-card__adviesprijs">
+							Adviesprijs {displayAsCurrency(product.suggestedPrice)}
+						</span>
 					)}
 
 					<span className="product-card__verkoopprijs">
